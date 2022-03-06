be20441f966668ed2fe4a792dd88735e96996fd7
Paulo Alcantara <jpaulo.alcantara329@gmail.com>
2022-03-06 13:57:05 +0800

    Added Hello World

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    int* p;
+
+    a=14;
+    p=&a;
+
+    //value ni a, address ni a na di gumamit ng pointer
+    printf("%d %p\n", a, &a);
+    //address ni a through p, indirect operation ng variable a, and value ni a.
+    printf("%p %d %d\n", p, *p, a);
+}
