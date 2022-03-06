aab9d7bff141d832318776fad919623f773a8b98
Paulo Alcantara <jpaulo.alcantara329@gmail.com>
2022-03-06 14:00:14 +0800

    Added Hello World

--- main.c
+++ main.c
@@ -9,6 +9,7 @@ int main()
     a=14;
     p=&a;
 
+    printf("Hello World");
     //value ni a, address ni a na di gumamit ng pointer
     printf("%d %p\n", a, &a);
     //address ni a through p, indirect operation ng variable a, and value ni a.
